49d8a66634445e8dc421d529ed4bd3ad5e5c658b
Kieran Hayes <kcmhayes@gmail.com>
2019-05-28 21:06:52 -0700

    unnecessary comments removed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -24,25 +24,6 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-# def index(request):
-# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# 	template = loader.get_template('polls/index.html')
-# 	context = {
-# 		'latest_question_list': latest_question_list,
-# 	}
-# 	return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-# 	# try:
-# 	question = get_object_or_404(Question, pk=question_id)
-# 		#question = Question.objects.get(pk=question_id)
-# 	# except Question.DoesNotExist:
-# 	# 	raise Http404("Question does not exist")
-# 	return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
